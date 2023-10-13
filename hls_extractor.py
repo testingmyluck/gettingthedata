18f79a20954a5e9eb8ad9e3e45b1f16669e0a81c
testingmyluck <147545715+testingmyluck@users.noreply.github.com>
2023-10-13 11:37:26 +0500

    Update hls_extractor.py

--- hls_extractor.py
+++ hls_extractor.py
@@ -15,7 +15,7 @@ port = int(os.environ.get("PORT", 5000))
 # Configure CORS to allow requests from multiple websites
 CORS(app, resources={
     r"/extract_hls": {
-        "origins": ["https://hdxxx-videoz.blogspot.com", "https://www.hotnippy.com"]
+        "origins": "*"
     }
 })
 
