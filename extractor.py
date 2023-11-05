8cb30f68e02d8f632ee9ca18bcac1c3d7d30238b
testingmyluck <147545715+testingmyluck@users.noreply.github.com>
2023-11-05 11:29:38 +0500

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,69 +1,7 @@
-# netlify-functions/extractor.py
-import json
-import os
-from flask import Flask, request, jsonify
-from bs4 import BeautifulSoup
-import re
-import requests
-import random
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/.netlify/functions/extractor', methods=['GET'])
-def extract_hls():
-    try:
-        video_url = request.args.get('video_url')
-
-        if not video_url:
-            return jsonify({"error": "Video URL parameter is missing."}), 400
-
-        response = requests.get(video_url)
-
-        if response.status_code == 200:
-            video_page = BeautifulSoup(response.text, 'html.parser')
-
-            script_tags = video_page.find_all('script')
-
-            hls_url = ""
-            video_url_high = ""
-            mobile_show_inline = ""
-
-            for script_tag in script_tags:
-                script_text = script_tag.get_text()
-                if 'html5player.setVideoHLS' in script_text:
-                    match = re.search(r"html5player\.setVideoHLS\('([^']+)'\)", script_text)
-                    if match:
-                        hls_url = match.group(1)
-
-                if 'html5player.setVideoUrlHigh' in script_text:
-                    match = re.search(r"html5player\.setVideoUrlHigh\('([^']+)'\)", script_text)
-                    if match:
-                        video_url_high = match.group(1)
-
-            v_views_div = video_page.find('div', {'id': 'v-views'})
-            if v_views_div:
-                mobile_show_inline_element = v_views_div.find('strong', {'class': 'mobile-show-inline'})
-                if mobile_show_inline_element:
-                    mobile_show_inline = mobile_show_inline_element.get_text().strip()
-
-            rating_good_perc = str(random.randint(70, 100)) + '%'
-
-            video_info = {
-                "hls_url": hls_url,
-                "video_url_high": video_url_high,
-                "mobile_show_inline": mobile_show_inline,
-                "rating_good_perc": rating_good_perc,
-            }
-
-            return jsonify(video_info), 200
-
-        else:
-            return jsonify({"error": "Failed to retrieve the video page."}), response.status_code
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
